46ff93473f689794db90c5ffb62c06f2ebb15f69
RockyChennn <wenwuchen1948@163.com>
2021-01-25 23:12:44 +0800

    fix: 修复处理中留存分类标签问题

--- K-means.py
+++ K-means.py
@@ -5,12 +5,6 @@ import random
 import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
 
-# 以下包为生成excel所用
-# import xlwt
-# import datetime
-# import os
-# import shutil
-
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
@@ -32,7 +26,7 @@ print("数据集类别数：", k)
 def load_data():
     points = np.loadtxt(data_path, delimiter=" ")
     print("实例个数：", len(points))
-    columns = np.shape(points)[1] - 1 # 除去分类标签为实际的特征个数
+    columns = np.shape(points)[1] - 1  # 除去分类标签为实际的特征个数
     return points, columns
 
 
@@ -48,11 +42,11 @@ def getDistanceMatrix(data, centerPoints):
     for i in range(len(data)):
         distanceMatrix.append([])
         for j in range(k):
-            distance = sum((data[i, :columns] - centerPoints[j, :]) ** 2)
+            distance = sum((data[i, :] - centerPoints[j, :])**2)
             distanceMatrix[i].append(
                 math.floor(math.sqrt(distance) * 100) / 100)
     for i in range(len(distanceMatrix)):
-        wcss += min(distanceMatrix[i]) ** 2
+        wcss += min(distanceMatrix[i])**2
     data_wcss.append(wcss)
     return np.asarray(distanceMatrix)
 
@@ -85,7 +79,7 @@ def center(data, clusterRes):
         avg_sum = sum / len(data[idx])
         centerNow.append(avg_sum)
     centerNow = np.asarray(centerNow)
-    return centerNow[:, 0:columns]
+    return centerNow
 
 
 def classfy(data, centerPoints):
@@ -141,98 +135,13 @@ def plotWCSS(data):
     plt.show()
 
 
-# def recordResult(accuracy):
-#     """
-#     记录数据
-#     :return: 在 result 目录下生成 excel 文件
-#     """
-#     workbook = xlwt.Workbook(encoding='utf-8')
-#     worksheet = workbook.add_sheet(data_name)
-#
-#     worksheet.write(0, 0, "数据集名称")
-#     worksheet.write(0, 1, data_name)
-#     worksheet.write(1, 0, "类别数")
-#     worksheet.write(1, 1, k)
-#     worksheet.write(2, 0, "实例个数")
-#     worksheet.write(2, 1, len(data))
-#     worksheet.write(3, 0, "聚类准确率")
-#     worksheet.write(3, 1, accuracy)
-#
-#     nowday = str(datetime.datetime.now()).replace(":", "-")[5:19]
-#     file_name = data_name + '(' + nowday + ").xls"
-#     workbook.save(file_name)
-#     aa = os.getcwd()
-#     # 获取当前文件路径
-#     file_path = os.path.join(aa, file_name)
-#     # 移动文件到E盘地方
-#     target_path = "./result"
-#     # 使用shutil包的move方法移动文件
-#     shutil.move(file_path, target_path)
-
-
-# def evaluate(clusterResult, index):
-#     total = len(index)
-#     group = []
-#     groupNum = []
-#     groupRecord = []
-#     for idx in set(index):
-#         currentRange = clusterResult[np.where(index == idx)]
-#         numInRange = set(currentRange)
-#         count = 0
-#         nowIndex = 0
-#         for num in numInRange:
-#             nowCount = len(currentRange[np.where(currentRange == num)])
-#             groupRecord.append([idx, num, nowCount])
-#             if nowCount >= count:
-#                 count = nowCount
-#                 nowIndex = num
-#         group.append(count)
-#         groupNum.append(nowIndex)
-#     accuracy = math.floor(np.sum(group) / total * 10000) / 100
-#     if len(set(groupNum)) == k:
-#         print("聚类结果：", groupNum)
-#         # clusterResult = np.asarray(list(map(int, clusterResult - 100)))
-#         # i = 0
-#         # for idx in groupNum:
-#         #     print(i, idx)
-#         #     if i != idx:
-#         #         clusterResult[np.where(clusterResult == idx)] - 100 - (idx - i)
-#         #     else:
-#         #         clusterResult[np.where(clusterResult == idx)] - 100
-#         #     i += 1
-#     else:
-#         # 类别重复的情况下，提示问题
-#         # print("聚类结果：", groupNum)
-#         print("聚类结果：", groupNum)
-#         print("纠错-聚类过程记录", groupRecord)
-#         # accuracy = findResult(groupRecord)
-#     # recordResult(accuracy) # 记录聚类结果
-#     return accuracy
-
-
-# def findResult(record):
-#     """
-#     动态规划，根据过程记录得到最佳可行解
-#     :param record: 数组，过程记录
-#     :return:最佳可行解
-#     """
-#     maplist = {}
-#     for i in range(k):
-#         maplist[i] = {}
-#     for item in range(len(np.asarray(record))):
-#         classNum, groupNum, count = record[item][0], record[item][1], record[
-#             item][2]
-#         maplist[classNum][groupNum] = count
-#     for i in maplist:
-#         print(maplist[i].keys())
-
-
 if __name__ == '__main__':
     data, columns = load_data()
     index = np.asarray(list(map(int, np.asarray(data[:, columns]) - 1)))
-    data_wcss = [] # 组内平方和变化曲线
+    data = data[:, 0:columns] # 去除索引，只留数据
+    data_wcss = []  # 组内平方和变化曲线
 
-    centerPoints = np.asarray(random.sample(data[:, 0:columns].tolist(), k))  # 随机取k个质心
+    centerPoints = np.asarray(random.sample(data.tolist(), k))  # 随机取k个质心
     err, centerNow, clusterRes = classfy(data, centerPoints)
     while np.any(abs(err) > 0.00001):
         err, centerNow, clusterRes = classfy(data, centerNow)  # 未满足收敛条件，继续聚类
@@ -242,11 +151,3 @@ if __name__ == '__main__':
     # 调整兰德指数计算得分基本用法
     score = metrics.adjusted_rand_score(index, clusterResult)
     print("兰德指数", score)
-
-    # plotWCSS(data_wcss)
-
-    # evaluate(clusterResult, index)
-    # print("聚类准确率：", evaluate(clusterResult, index), "%")
-
-    # plotRes(data, index)  # 原数据分布
-    # plotRes(data, clusterResult)  # 聚类结果可视化
